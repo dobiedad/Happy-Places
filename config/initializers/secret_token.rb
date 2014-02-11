c13bf4a3bf816a490bfc19c7ffde0e53a22578be
Levan Mdivani <leomdiv@gmail.com>
2014-02-11 14:54:26 +0000

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Happy1App::Application.config.secret_key_base = '000acc7061f3e9b64db9b48c68b1b92e3e9cedb7eda340e290cdc9b1296da82cb173789500f3c5525566a412a083f949bc836e31e40ceb2241586848cff2df70'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Happy1App::Application.config.session_store :cookie_store, key: '_happy1_app_session'
